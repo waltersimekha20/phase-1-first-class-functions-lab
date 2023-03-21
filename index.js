e06203cfc8a7fffdbd6c938fe4033cfd3be1e524
waltersimekha <waltersimekha@gmail.com>
2023-03-21 08:16:39 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Code your solution in this file!
+const returnFirstTwoDrivers = array => array.slice(0, 2);
+
+const returnLastTwoDrivers = array => array.slice(array.length - 2, array.length);
+
+const selectingDrivers = [
+    returnFirstTwoDrivers,
+    returnLastTwoDrivers
+];
+
+const createFareMultiplier = integer => () => integer * 5
+
+const var1 = createFareMultiplier()
+
+const fareDoubler = var1 => var1 * 2
+
+const fareTripler = var1 => var1 * 3
+
+function selectDifferentDrivers (arrayOfDrivers, driverFunction) {
+    return driverFunction(arrayOfDrivers);
+}
+Footer
